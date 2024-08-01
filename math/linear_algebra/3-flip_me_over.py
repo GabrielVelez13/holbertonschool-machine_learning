f6dd0d4330cbdf38ae6d35ecca77c17e2bc9b136
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-08-01 14:58:12 -0400

    Adding task 2 - 4

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Transpose a matrix
+""" Transpose a matrix """
 
 
 def matrix_transpose(matrix: list) -> list:
@@ -8,5 +8,6 @@ def matrix_transpose(matrix: list) -> list:
     :param matrix: must be a list.
     :return: Returns a transpose list of the matrix
     """
-    transposed = [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
+    transposed = [[matrix[j][i] for j in range(len(matrix))]
+                  for i in range(len(matrix[0]))]
     return transposed
