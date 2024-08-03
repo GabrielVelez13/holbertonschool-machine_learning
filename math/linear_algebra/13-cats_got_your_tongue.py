f4035a06032d3e6711ca7b9e9e160679b1c99675
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-08-02 23:01:23 -0400

    Fixing the last files

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -4,5 +4,8 @@ import numpy as np
 
 
 def np_cat(mat1, mat2, axis=0):
-    """ Using numpy's concatenate to do its name same taking into account the axis """
+    """
+    Using numpy's concatenate to do its
+    name same taking into account the axis
+    """
     return np.concatenate((mat1, mat2), axis=axis)
