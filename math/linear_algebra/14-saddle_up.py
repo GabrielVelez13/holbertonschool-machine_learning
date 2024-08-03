d25232c15db6de1e309287696b74f13f9890602e
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-08-02 22:58:19 -0400

    Fixing the last files

--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-"""  """
+""" Use matrix multiplication """
 import numpy as np
 
 
 def np_matmul(mat1, mat2):
+    """ Multiplying matrices using numpy's matmul """
+    return np.matmul(mat1, mat2)
