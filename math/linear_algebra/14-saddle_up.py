d0e68cbe11b201c160e685b703917e385f933e81
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-08-02 23:05:29 -0400

    Added a comment about the shorthand of matmul

--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -5,4 +5,5 @@ import numpy as np
 
 def np_matmul(mat1, mat2):
     """ Multiplying matrices using numpy's matmul """
+    """ mat1 @ mat2 this is the shorthand for matmul"""
     return np.matmul(mat1, mat2)
