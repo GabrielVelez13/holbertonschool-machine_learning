bfc0b773d7e2d79f560cd9780c746d649ca33468
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-08-08 16:27:38 -0400

    Believe to have fixed the issue

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -7,7 +7,7 @@ def poly_derivative(poly):
     if not isinstance(poly, list):
         return None
 
-    if len(poly) == 1 or len(poly) == 0:
+    if len(poly) == 1:
         return None
 
     return [i * num for i, num in enumerate(poly)][1:]
