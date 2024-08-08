c4d81e0f374a4512681b71123952acd158fd5396
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-08-08 16:21:31 -0400

    is instance

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -4,10 +4,13 @@
 
 def poly_derivative(poly):
     """ List comprehension and slicing to return the derivative """
-    if poly == list:
+    if not isinstance(poly, list):
         return None
 
     if len(poly) == 1:
         return [0]
 
     return [i * num for i, num in enumerate(poly)][1:]
+
+
+print(isinstance([], list))
