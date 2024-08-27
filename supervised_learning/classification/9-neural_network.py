8dfe40f61da1703d90eb455fa1cb58bdcb975a0a
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-08-27 16:00:48 -0400

    Adding task 9

---
+++ supervised_learning/classification/9-neural_network.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+""" Beginnings of a neural network """
+import numpy as np
+
+
+class NeuralNetwork:
+    """ Class where it happens """
+    def __init__(self, nx, nodes):
+        """
+        init the weights, biases, and activations of the hidden
+        layer and the output layer
+        """
+        if not isinstance(nx, int):
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        if not isinstance(nodes, int):
+            raise TypeError('nodes must be an integer')
+        if nodes < 1:
+            raise ValueError('nodes must be a positive integer')
+
+        self.__W1 = np.random.normal(size=(nodes, nx))
+        self.__b1 = np.zeros((1, nodes)).T
+        self.__A1 = 0
+        self.__W2 = np.random.normal(size=(1, nodes))
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        """ Returns desired private attribute """
+        return self.__W1
+
+    @property
+    def b1(self):
+        """ Returns desired private attribute """
+        return self.__b1
+
+    @property
+    def A1(self):
+        """ Returns desired private attribute """
+        return self.__A1
+
+    @property
+    def W2(self):
+        """ Returns desired private attribute """
+        return self.__W2
+
+    @property
+    def b2(self):
+        """ Returns desired private attribute """
+        return self.__b2
+
+    @property
+    def A2(self):
+        """ Returns desired private attribute """
+        return self.__A2
