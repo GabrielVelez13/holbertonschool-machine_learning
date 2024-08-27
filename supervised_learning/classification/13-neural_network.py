379cb33a6838581620cbc6e3181314918e58af2b
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-08-27 18:23:20 -0400

    adding task 13

--- supervised_learning/classification/13-neural_network.py
+++ supervised_learning/classification/13-neural_network.py
@@ -102,16 +102,3 @@ class NeuralNetwork:
         self.__b2 -= alpha * db2
         self.__W1 -= alpha * dW1
         self.__b1 -= alpha * db1
-
-lib_train = np.load('data/Binary_Train.npz')
-X_3D, Y = lib_train['X'], lib_train['Y']
-X = X_3D.reshape((X_3D.shape[0], -1)).T
-
-np.random.seed(0)
-nn = NeuralNetwork(X.shape[0], 3)
-A1, A2 = nn.forward_prop(X)
-nn.gradient_descent(X, Y, A1, A2, 0.5)
-print(nn.W1)
-print(nn.b1)
-print(nn.W2)
-print(nn.b2)
