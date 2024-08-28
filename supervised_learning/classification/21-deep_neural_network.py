08e396921d650268fabecc9cd541c0e0cdd5c783
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-08-28 16:33:15 -0400

    adding task 21

---
+++ supervised_learning/classification/21-deep_neural_network.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+""" Starting deep neural networks """
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """ Deep class """
+
+    def __init__(self, nx, layers):
+        """
+        Validata nx and layers
+        init L, cache and weights using he et al. method
+        """
+        if not isinstance(nx, int):
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        if not isinstance(layers, list) or not layers:
+            raise TypeError('layers must be a list of positive '
+                            'integers')
+        if layers[-1] < 1:
+            raise TypeError('layers must be a list of positive '
+                            'integers')
+
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
+
+        for i in range(self.L):
+            if i == 0:
+                self.weights['W1'] = (np.random.randn(layers[0], nx) *
+                                      np.sqrt(2 / nx))
+            else:
+                self.weights[f'W{i + 1}'] = (
+                        np.random.randn(layers[i], layers[i - 1]) *
+                        np.sqrt(2 / layers[i - 1]))
+            self.weights[f'b{i + 1}'] = np.zeros((layers[i], 1))
+
+    @property
+    def L(self):
+        """ Getter function """
+        return self.__L
+
+    @property
+    def cache(self):
+        """ Getter function """
+        return self.__cache
+
+    @property
+    def weights(self):
+        """ Getter function """
+        return self.__weights
+
+    @staticmethod
+    def sigmoid(X):
+        """ Returns the sigmoid function """
+        return 1 / (1 + np.exp(-X))
+
+    def forward_prop(self, X):
+        """ Forward propagating the deep network """
+        self.__cache['A0'] = X
+        for i in range(self.L):
+            z = np.dot(self.weights[f'W{i + 1}'],
+                       self.cache[f'A{i}']) + self.weights[f'b{i + 1}']
+            self.__cache[f'A{i + 1}'] = self.sigmoid(z)
+        return self.__cache[f'A{self.L}'], self.__cache
+
+    def cost(self, Y, A):
+        """ Calculates the cost function using logistic regression """
+        m = Y.shape[1]
+        return -np.sum(Y * np.log(A) + (1 - Y) * (np.log(1.0000001 -
+                                                         A))) / m
+
+    def evaluate(self, X, Y):
+        """ Evaluates outputs prediction and cost """
+        self.forward_prop(X)
+        cost = self.cost(Y, self.cache[f'A{self.L}'])
+        return np.where(self.cache[f'A{self.L}'] >= .5, 1, 0), cost
+
+    def gradient_descent(self, Y, cache, alpha=0.05):
+        """ Calculates the gradient descent """
+        m = Y.shape[1]
+        for i in reversed(range(1, self.L + 1)):
+            A = self.cache[f'A{i}']
+            if i == self.L:
+                de = A - Y
+            else:
+                de = np.dot(self.weights[f'W{i + 1}'].T, de) * A * (1
+                                                                  - A)
+            dz = np.dot(de, cache[f'A{i - 1}'].T) / m
+            db = np.sum(de, axis=1, keepdims=True) / m
+            self.__weights[f'W{i}'] -= alpha * dz
+            self.__weights[f'b{i}'] -= alpha * db
