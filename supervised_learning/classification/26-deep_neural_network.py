74705e8fa459b192bf1865e0371f7268350ac7e5
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-08-28 19:37:19 -0400

    adding task 26

--- supervised_learning/classification/26-deep_neural_network.py
+++ supervised_learning/classification/26-deep_neural_network.py
@@ -107,11 +107,12 @@ class DeepNeuralNetwork:
             raise TypeError('alpha must be a float')
         if alpha <= 0:
             raise ValueError('alpha must be positive')
-        if not isinstance(step, int):
-            raise TypeError('step must be an integer')
-        if (step <= 0 or step >= iterations) and verbose:
-            raise ValueError('step must be a positive integer and '
-                             '<= iterations')
+        if verbose is True or graph is True:
+            if not isinstance(step, int):
+                raise TypeError("step must be an integer")
+            if step <= 0 or step > iterations:
+                raise ValueError(
+                    "step must be positive and <= iterations")
 
         costs = []
 
