f3643098e78baf3321d80d205f6195dd7865c1db
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-09-06 18:28:36 -0400

    Adding task 2 - forward prop throught the custom layers

---
+++ supervised_learning/tensorflow/2-forward_prop.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Forward propagating through layers """
+import tensorflow.compat.v1 as tf
+create_layer = __import__('1-create_layer').create_layer
+
+
+def forward_prop(x, layer_size=[], activation=[]):
+    """ going throughout a certain number of layers
+    and activations to forward prop """
+    for size, activation in zip(layer_size, activation):
+        pred = create_layer(x, size, activation)
+    return pred
