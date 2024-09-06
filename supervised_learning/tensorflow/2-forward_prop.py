2d022ab804c4d5b32d64e714faa2c7d0c6abec01
GabrielVelez13 <gabriel.velez131@gmail.com>
2024-09-06 18:30:35 -0400

    Adding task 2 - forward prop throught the custom layers

--- supervised_learning/tensorflow/2-forward_prop.py
+++ supervised_learning/tensorflow/2-forward_prop.py
@@ -7,7 +7,7 @@ create_layer = __import__('1-create_layer').create_layer
 def forward_prop(x, layer_size=[], activation=[]):
     """ going throughout a certain number of layers
     and activations to forward prop """
-    prev = x
+    pred = x
     for size, activation in zip(layer_size, activation):
-        pred = create_layer(prev, size, activation)
+        pred = create_layer(pred, size, activation)
     return pred
